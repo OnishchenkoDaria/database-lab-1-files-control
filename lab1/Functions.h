c79c9888f981abe5e773f123ae1c9f06099dbb49
Daria Onishchenko <daria.onishchenko.57@gmail.com>
2024-02-21 00:27:14 +0200

    the indexNodes are recordedd in separate file

--- lab1/Functions.h
+++ lab1/Functions.h
@@ -8,6 +8,7 @@
 #include "indexTableList.h"
 
 indexTableList Table;
+const int MAXrecordsSIZE = 20;
 
 void loadTheIndex() {
 	
@@ -41,6 +42,17 @@ void writeAudienceToFile(Audience obj) {
 	//outFile.close();
 }
 
+void AddNewIndexRecord(indexTable obj) {
+	ofstream outFile("index.txt", ios::app);
+
+	if (!outFile) {
+		cout << "Error opening file!" << endl;
+		return;
+	}
+
+	outFile << obj.getAudienceNumber() << " " << obj.getAudienceLink() << endl;
+}
+
 void AddToIndexTable(indexTable obj) {
 	Table.addNewItem(obj);
 	Table.showAllList();
@@ -52,6 +64,7 @@ void AddNewAudience() {
 	writeAudienceToFile(aud);
 	indexTable obj(aud.getNumber(), &aud);
 	AddToIndexTable(obj);
+	AddNewIndexRecord(obj);
 }
 
 /// THE ADD NEW MASTER --- FINISH
